9d54c86d220ee6cdb49974da7e3a7448a1104b53
William L <w-lindvall@outlook.com>
2020-05-08 15:38:31 -0700

    fix width breakpoint for wheel layout triggering late

--- src/hooks/use-colors.js
+++ src/hooks/use-colors.js
@@ -32,7 +32,7 @@ export const useColors = circleSelector => {
     elems.forEach(el => {
       const x =
         100 * Math.cos(angleOffset) +
-        (width - (width > 800 ? 500 : 0) - WHEEL_CIRCLE_SIZE) / 2
+        (width - (width > 799 ? 500 : 0) - WHEEL_CIRCLE_SIZE) / 2
       const y = 100 * Math.sin(angleOffset) - 50
       el.style.position = 'absolute'
       el.style.left = x + 'px'
